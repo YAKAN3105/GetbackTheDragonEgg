721a6934eeffeade0a99368d528eb79e4b266fd8
Emoruworld <166367479+Emoruworld@users.noreply.github.com>
2024-07-23 19:04:09 +0900

    yusei0723

---
+++ DragonEgg(2D)/Assets/yusei_BGM,SE/MusicManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    AudioSource source;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+//        source = GetComponent<WarningSiren04>();
+
+        source.Stop(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DragonEgg(2D)/Assets/yusei_BGM,SE/warningManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class warningManager : MonoBehaviour
+{
+    GameObject Warning;
+    Image GetImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Warning = GameObject.Find("warning");
+
+ //       Image GetImage = 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
